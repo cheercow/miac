893fb8a368c0f82d0f31f0742d3390a15e386894
rgayanov <rgayanov@htsts.ru>
2021-06-18 23:41:15 +0500

    feat(h24) - add models

--- monitoring_system/models.py
+++ monitoring_system/models.py
@@ -1,3 +1,33 @@
+import datetime
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class RegistryAddress(models.Model):
+    uid = models.UUIDField(default=uuid.UUID, null=False, db_index=True)
+    region = models.CharField(null=True)
+    city = models.CharField(null=True)
+    street = models.CharField(null=True)
+    house = models.CharField(null=True)
+    apartment = models.CharField(null=True)
+
+    class Meta:
+        db_table = 'registry_address'
+
+
+class Patient(models.Model):
+    uid = models.UUIDField(default=uuid.UUID, null=False, db_index=True)
+    name = models.CharField(max_length=255)
+    start_date = models.DateField(null=True, default=datetime.datetime.now())
+    end_date = models.DecimalField(null=True)
+    sex = models.BooleanField(null=True)  # female = true, male = false
+    date_of_birth = models.DateField(null=True)
+    registry_address = models.ForeignKey(RegistryAddress, null=True, on_delete=models.PROTECT)
+    phone = models.CharField(null=True)
+    oms = models.CharField(null=True)
+    insurance = models.CharField(null=True)
+    passport = models.CharField(null=True)
+
+    class Meta:
+        db_table = 'patients'
