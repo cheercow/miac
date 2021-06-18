635f1eb506ad2e484eaeff578c06b389478413a9
rgayanov <rgayanov@htsts.ru>
2021-06-19 00:04:20 +0500

    fix(main)- fix  models

--- monitoring_system/models.py
+++ monitoring_system/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 class Doctor(models.Model):
     uid = models.UUIDField(default=uuid.UUID, null=False, db_index=True)
-    name = models.CharField(null=True)
-    department = models.CharField(null=True)
+    name = models.CharField(null=True,max_length=255)
+    department = models.CharField(null=True,max_length=255)
 
     class Meta:
         db_table = 'doctors'
@@ -15,11 +15,11 @@ class Doctor(models.Model):
 
 class RegistryAddress(models.Model):
     uid = models.UUIDField(default=uuid.UUID, null=False, db_index=True)
-    region = models.CharField(null=True)
-    city = models.CharField(null=True)
-    street = models.CharField(null=True)
-    house = models.CharField(null=True)
-    apartment = models.CharField(null=True)
+    region = models.CharField(null=True,max_length=255)
+    city = models.CharField(null=True,max_length=255)
+    street = models.CharField(null=True,max_length=255)
+    house = models.CharField(null=True,max_length=255)
+    apartment = models.CharField(null=True,max_length=255)
 
     class Meta:
         db_table = 'registry_address'
@@ -28,15 +28,15 @@ class RegistryAddress(models.Model):
 class Patient(models.Model):
     uid = models.UUIDField(default=uuid.UUID, null=False, db_index=True)
     name = models.CharField(max_length=255)
-    start_date = models.DateField(null=True, default=datetime.datetime.now())
-    end_date = models.DecimalField(null=True)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
     sex = models.BooleanField(null=True)  # female = true, male = false
     date_of_birth = models.DateField(null=True)
     registry_address = models.ForeignKey(RegistryAddress, null=True, on_delete=models.PROTECT)
-    phone = models.CharField(null=True)
-    oms = models.CharField(null=True)
-    insurance = models.CharField(null=True)
-    passport = models.CharField(null=True)
+    phone = models.CharField(null=True,max_length=255)
+    oms = models.CharField(null=True,max_length=255)
+    insurance = models.CharField(null=True,max_length=255)
+    passport = models.CharField(null=True,max_length=255)
     doctor = models.ForeignKey(Doctor, null=True, on_delete=models.PROTECT)
 
     class Meta:
@@ -45,7 +45,7 @@ class Patient(models.Model):
 
 class Measurement(models.Model):
     uid = models.UUIDField(default=uuid.UUID, null=False, db_index=True)
-    date = models.DateField(null=True, default=datetime.datetime.now())
+    date = models.DateField(null=True)
     upper_point = models.IntegerField(null=True)
     lower_point = models.IntegerField(null=True)
     patient = models.ForeignKey(Patient, null=True, on_delete=models.PROTECT)
